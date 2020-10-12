a39fcfbab63375a8c6ba72b4410ef3f5ae5cb915
faker <skt.faker.ccj@gmail.com>
2020-10-12 17:38:36 +0800

    go

--- src/github.com/Hide-on-bush2/selpg/selpg.go
+++ src/github.com/Hide-on-bush2/selpg/selpg.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"os/exec"
+
 	"github.com/spf13/pflag"
 )
 
@@ -30,7 +33,7 @@ func get_arg(args *selpg_args) {
 	if len(file_name) > 0 {
 		args.in_filename = string(file_name[0])
 	} else {
-		args.in_filename = ""sa
+		args.in_filename = ""
 	}
 
 }
@@ -43,9 +46,9 @@ func print_parameter(args *selpg_args) {
 	fmt.Println("end page: ", args.end_page)
 	fmt.Println("lines per page: ", args.lines_per_page)
 	if args.page_type {
-		fmt.Println("page type: ")
+		fmt.Println("page type: f")
 	} else {
-		fmt.Println("page type: ")
+		fmt.Println("page type: l")
 	}
 	fmt.Println("dest: ", args.dest)
 	fmt.Println("in filename: ", args.in_filename)
@@ -54,35 +57,34 @@ func print_parameter(args *selpg_args) {
 /*
 打印错误信息
 */
-func args_error(message string){
-	fmt.Fprintf(os.Stderr, "[Error]:" + message)
+func Error(message string) {
+	fmt.Fprintf(os.Stderr, "[Error]:"+message)
 	os.Exit(1)
 }
-
 
 /*
 检查参数格式
 */
-func check_args(args *selpg_args){
+func check_args(args *selpg_args) {
 	//必须有开始页和结束页并且开始页和结束页不能小于等于0并且结束页不能小于开始页
 	if args.start_page <= 0 || args.end_page <= 0 {
-		args_error("Invalid start page and end page parameter\n")
+		Error("Invalid start page and end page parameter\n")
 	}
 	//-l和-f参数不能同时输入
 	if args.lines_per_page != 72 && args.page_type {
-		args_error("Cannot input -l and -f at the same time\n")
+		Error("Cannot input -l and -f at the same time\n")
 	}
 	//自定义页长不可以小于等于0
 	if args.lines_per_page <= 0 {
-		args_error("the lines per page cannot less than zero\n")
+		Error("the lines per page cannot less than zero\n")
 	}
-	
+
 }
 
 /*
 执行命令
 */
-func exec(args *selpg_args){
+func Exec(args *selpg_args) {
 	//检查输入文件名，如果没有输入文件名从标准输入流中读取
 	//如果有输入文件名，则从输入文件名中读取
 	//检查输出文件名，如果没有输出文件名则打印到标准输出流中
@@ -90,8 +92,111 @@ func exec(args *selpg_args){
 	var in_file *os.File
 	if args.in_filename == "" {
 		in_file = os.Stdin
-	}else{
-		
+	} else {
+		//检查文件是否存在
+		_, exist_file := os.Stat(args.in_filename)
+		if os.IsNotExist(exist_file) {
+			Error("input file not exist\n")
+		}
+
+		var err error
+		in_file, err = os.Open(args.in_filename)
+
+		//检查文件是否打开
+		if err != nil {
+			Error("file not open\n")
+		}
+	}
+
+	if args.dest == "" {
+		output(os.Stdout, in_file, args)
+	} else {
+		cmd := exec.Command("lp", "-d"+args.dest)
+		fout, err := cmd.StdinPipe()
+		if err != nil {
+			Error("StdinPipe")
+		}
+
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		errStart := cmd.Run()
+		if errStart != nil {
+			Error("CMD Run")
+		}
+
+		output(fout, in_file, args)
+	}
+
+}
+
+/*
+接受dest和in_file的文件句柄，将in_file中的特定内容输出到dest中
+*/
+
+func output(fout interface{}, in_file *os.File, args *selpg_args) {
+	lines := 0
+	pages := 1
+	buf := bufio.NewReader(in_file)
+
+	for true {
+		var line string
+		var err error
+
+		if args.page_type {
+			//以换页符定义翻页
+			line, err = buf.ReadString('\f')
+			pages += 1
+		} else {
+			//自定义页长
+			line, err = buf.ReadString('\n')
+			lines += 1
+
+			if lines > args.lines_per_page {
+				pages += 1
+				lines = 1
+			}
+		}
+
+		//读到文件结尾
+		if err == io.EOF {
+			break
+		}
+
+		//检查出错
+		if err != nil {
+			Error("file read error")
+		}
+
+		//如果在需要输出的范围内，则输出
+		if pages >= args.start_page && pages <= args.end_page {
+			var output_error error
+
+			stdOutput, ok := fout.(*os.File)
+			if ok {
+				_, output_error = fmt.FPrintf(stdOutput, "%s", line)
+				if output_error != nil {
+					Error("output file error\n")
+				}
+				continue
+			}
+
+			pipeOutput, ok := fout.(io.WriterCLoser)
+			if ok {
+				_, output_error = pipeOutput.Write([]byte(line))
+				if output_error != nil {
+					Error("output file error\n")
+				}
+				continue
+			}
+
+			Error("fout type error\n")
+		}
+	}
+
+	if pages < args.start_page {
+		Error("start page too large\n")
+	} else if pages < args.end_page {
+		Error("end page too large\n")
 	}
 }
 
@@ -100,4 +205,5 @@ func main() {
 	get_arg(&args)
 	check_args(&args)
 	print_parameter(&args)
+	Exec(&args)
 }
