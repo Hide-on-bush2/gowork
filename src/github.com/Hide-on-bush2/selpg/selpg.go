45fb8c79510baddaf78fee93b532da4b9bbd6867
faker <skt.faker.ccj@gmail.com>
2020-10-12 12:34:24 +0800

    go

--- src/github.com/Hide-on-bush2/selpg/selpg.go
+++ src/github.com/Hide-on-bush2/selpg/selpg.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-
+	"os"
 	"github.com/spf13/pflag"
 )
 
@@ -15,6 +15,9 @@ type selpg_args struct {
 	dest           string
 }
 
+/*
+从输入流中获取参数保存到sel_args结构体中
+*/
 func get_arg(args *selpg_args) {
 	pflag.IntVarP(&(args.start_page), "start_page", "s", -1, "start page")
 	pflag.IntVarP(&(args.end_page), "end_page", "e", -1, "end page")
@@ -32,6 +35,9 @@ func get_arg(args *selpg_args) {
 
 }
 
+/*
+打印参数列表
+*/
 func print_parameter(args *selpg_args) {
 	fmt.Println("start page: ", args.start_page)
 	fmt.Println("end page: ", args.end_page)
@@ -45,8 +51,53 @@ func print_parameter(args *selpg_args) {
 	fmt.Println("in filename: ", args.in_filename)
 }
 
+/*
+打印错误信息
+*/
+func args_error(message string){
+	fmt.Fprintf(os.Stderr, "[Error]:" + message)
+	os.Exit(1)
+}
+
+
+/*
+检查参数格式
+*/
+func check_args(args *selpg_args){
+	//必须有开始页和结束页并且开始页和结束页不能小于等于0并且结束页不能小于开始页
+	if args.start_page <= 0 || args.end_page <= 0 {
+		args_error("Invalid start page and end page parameter\n")
+	}
+	//-l和-f参数不能同时输入
+	if args.lines_per_page != 72 && args.page_type {
+		args_error("Cannot input -l and -f at the same time\n")
+	}
+	//自定义页长不可以小于等于0
+	if args.lines_per_page <= 0 {
+		args_error("the lines per page cannot less than zero\n")
+	}
+	
+}
+
+/*
+执行命令
+*/
+func exec(args *selpg_args){
+	//检查输入文件名，如果没有输入文件名从标准输入流中读取
+	//如果有输入文件名，则从输入文件名中读取
+	//检查输出文件名，如果没有输出文件名则打印到标准输出流中
+	//如果有输入文件名，则写入到输出文件中
+	var in_file *os.File
+	if args.in_filename == "" {
+		in_file = os.Stdin
+	}else{
+		
+	}
+}
+
 func main() {
 	var args selpg_args
 	get_arg(&args)
+	check_args(&args)
 	print_parameter(&args)
 }
