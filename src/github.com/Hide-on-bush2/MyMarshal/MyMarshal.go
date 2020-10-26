466ba52d3b5efb9b4a6974ca7d00ad5b41dc6c00
faker <skt.faker.ccj@gmail.com>
2020-10-26 23:10:21 +0800

    go

--- src/github.com/Hide-on-bush2/MyMarshal/MyMarshal.go
+++ src/github.com/Hide-on-bush2/MyMarshal/MyMarshal.go
@@ -3,13 +3,11 @@ package MyMarshal
 import (
 	"fmt"
 	"reflect"
-	"strconv"
 )
 
 func JsonMarshal(v interface{}) ([]byte, error) {
-	m, _ := struct2str(v)
-	fmt.Println(m)
-	return nil, nil
+	json, _ := struct2str(v)
+	return []byte(json), nil
 }
 
 func struct2str(v interface{}) (string, error) {
@@ -20,14 +18,24 @@ func struct2str(v interface{}) (string, error) {
 
 	switch obj_type.Kind() {
 	case reflect.String:
-		fmt.Printf("%v:%s\n", obj_type.Name, obj.String())
-		str = obj_type.Name + " : " + obj.String()
+		str = fmt.Sprintf("%v", v)
+		// fmt.Printf(str)
+		// fmt.Printf("%v:%s\n", obj_type.Name, obj.String())
+		// str = obj_type.Name + " : " + obj.String()
 	case reflect.Int:
-		fmt.Printf("%v:%s\n", obj_type.Name, strconv.FormatInt(obj.Int(), 10))
-		str = obj_type.Name + " : " + strconv.FormatInt(obj.Int(), 10)
+		str = fmt.Sprintf("%v", v)
+		// fmt.Printf(str)
+		// fmt.Printf("%v:%s\n", obj_type.Name, strconv.FormatInt(obj.Int(), 10))
+		// str = obj_type.Name + " : " + strconv.FormatInt(obj.Int(), 10)
 	case reflect.Struct:
 		str += "{"
-
+		for i := 0; i < count; i++ {
+			// field := obj_type.Field(i)
+			val := obj.Field(i)
+			str += fmt.Sprintf("%v", val)
+			// fmt.Printf(t_str)
+		}
+		str += "}"
 	}
 
 	return str, nil
--- src/github.com/Hide-on-bush2/main/main.go
+++ src/github.com/Hide-on-bush2/main/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/Hide-on-bush2/MyMarshal"
+import (
+	"fmt"
+
+	"github.com/Hide-on-bush2/MyMarshal"
+)
 
 // func main() {
 // 	hide_on_bush := read_ini.Listen_methods{read_ini.MyListen}
@@ -15,7 +19,8 @@ type tmp struct {
 }
 
 func main() {
-	MyMarshal.JsonMarshal(tmp{"faker", "SKT", 23})
+	json, _ := MyMarshal.JsonMarshal(tmp{"Faker", "SKT", 10})
+	fmt.Println(string(json))
 	// t := tmp{"Faker", "SKT", 10}
 	// obj_type := reflect.TypeOf(t)
 	// t1 := reflect.TypeOf(obj_type.Field(0).Name)
