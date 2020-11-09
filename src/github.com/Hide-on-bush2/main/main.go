c9d7ed36c965ea9e7e3193cbb5b509bfcd00809a
faker <skt.faker.ccj@gmail.com>
2020-11-09 18:40:39 +0800

    faker

--- src/github.com/Hide-on-bush2/main/main.go
+++ src/github.com/Hide-on-bush2/main/main.go
@@ -3,13 +3,47 @@ package main
 import (
 	"fmt"
 
-	RxGo "github.com/Hide-on-bush2/rxgo"
+	"github.com/Hide-on-bush2/rxgo"
 )
 
 func main() {
-	RxGo.Just("Hello", "World", "!", "!", "!", "!", "Hello").Distinct().Subscribe(func(x string) {
+	// RxGo.Just("Hello", "World", "!", "!", "!", "!", "Hello").First(func(x interface{}) bool { return x == "World" }).Subscribe(func(x string) {
+	// 	fmt.Println(x)
+	// })
+
+	// RxGo.Just("Hello", "World", "!", "!", "!", "!", "Hello").Skip(3).Subscribe(func(x string) {
+	// 	fmt.Println(x)
+	// })
+
+	// RxGo.Just("Hello", "World", "!", "!", "!", "!", "Hello").Take(3).Subscribe(func(x string) {
+	// 	fmt.Println(x)
+	// })
+
+	// rxgo.Just("Hello1", "World", "!1", "!2", "!3", "!4", "Hello2").Debounce(time.Second).Subscribe(func(x string) {
+	// 	fmt.Println(x)
+	// })
+
+	// rxgo.Just("Hello1", "World", "!1", "!2", "!3", "!4", "Hello2").Last().Subscribe(func(x string) {
+	// 	fmt.Println(x)
+	// })
+	// rxgo.Just("Hello1", "World", "!1", "!2", "!3", "!4", "Hello2").Skiplast(3).Subscribe(func(x string) {
+	// 	fmt.Println(x)
+	// })
+	rxgo.Just("Hello1", "World", "!1", "!2", "!3", "!4", "Hello2").Takelast(3).Subscribe(func(x string) {
 		fmt.Println(x)
 	})
+	// rxgo.Just(0, 1, 2, 3, 4, 5, 6, 7, 8, 9).Map(func(x int) int {
+	// 	if x != 0 {
+	// 		time.Sleep(1 * time.Millisecond)
+	// 	}
+	// 	return x
+	// }).Debounce(2 * time.Millisecond).Subscribe(func(x int) {
+	// 	if x != 9 {
+	// 		fmt.Printf("error Debounce with %d\n", x)
+	// 		os.Exit(-1)
+	// 	}
+	// 	fmt.Printf("Debunce %d\n", x)
+	// })
 
 	// RxGo.Just("Hello", "World", "!", "!", "!", "!", "Hello").ElementAt("!").Subscribe(func(x string) {
 	// 	fmt.Println(x)
--- src/github.com/Hide-on-bush2/rxgo/transforms.go
+++ src/github.com/Hide-on-bush2/rxgo/transforms.go
@@ -8,6 +8,7 @@ import (
 	"context"
 	"reflect"
 	"sync"
+	"time"
 )
 
 var (
@@ -67,6 +68,65 @@ func (tsop transOperater) op(ctx context.Context, o *Observable) {
 		wg.Wait() //waiting all go-routines completed
 		o.closeFlow(out)
 	}()
+}
+
+func (tsop myOperater) op(ctx context.Context, o *Observable) {
+	// must hold defintion of flow resourcs here, such as chan etc., that is allocated when connected
+	// this resurces may be changed when operation routine is running.
+	in := o.pred.outflow
+	out := o.outflow
+	//fmt.Println(o.name, "operator in/out chan ", in, out)
+	var wg sync.WaitGroup
+
+	go func() {
+		end := false
+		for x := range in {
+			if end {
+				continue
+			}
+			// can not pass a interface as parameter (pointer) to gorountion for it may change its value outside!
+			xv := reflect.ValueOf(x)
+			// send an error to stream if the flip not accept error
+			if e, ok := x.(error); ok && !o.flip_accept_error {
+				o.sendToFlow(ctx, e, out)
+				continue
+			}
+			// scheduler
+			switch threading := o.threading; threading {
+			case ThreadingDefault:
+				if tsop.opFunc(ctx, o, xv, out) {
+					end = true
+				}
+			case ThreadingIO:
+				fallthrough
+			case ThreadingComputing:
+				wg.Add(1)
+				go func() {
+					defer wg.Done()
+					if tsop.opFunc(ctx, o, xv, out) {
+						end = true
+					}
+				}()
+			default:
+			}
+		}
+		if o.flip != nil {
+			buffer, ok := reflect.ValueOf(o.flip).Interface().([]reflect.Value)
+			if !ok {
+				panic("filter.go(op): buffer is not a slice")
+			}
+			for _, v := range buffer {
+				o.sendToFlow(ctx, v.Interface(), out)
+			}
+		}
+
+		wg.Wait() //waiting all go-routines completed
+		o.closeFlow(out)
+	}()
+}
+
+type myOperater struct {
+	opFunc func(ctx context.Context, o *Observable, item reflect.Value, out chan interface{}) (end bool)
 }
 
 func (parent *Observable) TransformOp(tf transformFunc) (o *Observable) {
@@ -357,7 +417,7 @@ func (parent *Observable) ElementAt(i interface{}) (o *Observable) {
 
 	o.flip_sup_ctx = ctx_sup
 	o.flip = fv.Interface()
-	o.operator = distinctOperater
+	o.operator = elementAtOperater
 	return o
 }
 
@@ -392,3 +452,301 @@ var elementAtOperater = transOperater{func(ctx context.Context, o *Observable, x
 
 	return
 }}
+
+//Homework, First
+
+func (parent *Observable) First(f interface{}) (o *Observable) {
+	// check validation of f
+	flag := false
+	fv := reflect.ValueOf(
+		func(x interface{}) bool {
+			if !flag && f.(func(interface{}) bool)(x) {
+				flag = true
+				return true
+			} else {
+				return false
+			}
+		})
+	inType := []reflect.Type{typeAny}
+	outType := []reflect.Type{typeBool}
+	b, ctx_sup := checkFuncUpcast(fv, inType, outType, true)
+	if !b {
+		panic(ErrFuncFlip)
+	}
+
+	o = parent.newTransformObservable("distinct")
+	o.flip_accept_error = checkFuncAcceptError(fv)
+
+	o.flip_sup_ctx = ctx_sup
+	o.flip = fv.Interface()
+	o.operator = firstOperater
+	return o
+}
+
+var firstOperater = transOperater{func(ctx context.Context, o *Observable, x reflect.Value, out chan interface{}) (end bool) {
+
+	fv := reflect.ValueOf(o.flip)
+	var params = []reflect.Value{x}
+	rs, skip, stop, e := userFuncCall(fv, params)
+
+	var item interface{} = rs[0].Interface()
+	if stop {
+		end = true
+		return
+	}
+	if skip {
+		return
+	}
+	if e != nil {
+		item = e
+	}
+
+	// send data
+	if !end {
+		if b, ok := item.(bool); ok && b {
+			end = o.sendToFlow(ctx, x.Interface(), out)
+		}
+		// if item != nil && item == x.Interface() {
+		// 	end = o.sendToFlow(ctx, x.Interface(), out)
+		// }
+		// end = o.sendToFlow(ctx, item, out)
+	}
+
+	return
+}}
+
+//Homework, Skip
+
+func (parent *Observable) Skip(count int) (o *Observable) {
+	// check validation of f
+	i := 0
+	fv := reflect.ValueOf(
+		func(x interface{}) bool {
+			i += 1
+			return i > count
+		})
+	inType := []reflect.Type{typeAny}
+	outType := []reflect.Type{typeBool}
+	b, ctx_sup := checkFuncUpcast(fv, inType, outType, true)
+	if !b {
+		panic(ErrFuncFlip)
+	}
+
+	o = parent.newTransformObservable("distinct")
+	o.flip_accept_error = checkFuncAcceptError(fv)
+
+	o.flip_sup_ctx = ctx_sup
+	o.flip = fv.Interface()
+	o.operator = skipOperater
+	return o
+}
+
+var skipOperater = transOperater{func(ctx context.Context, o *Observable, x reflect.Value, out chan interface{}) (end bool) {
+
+	fv := reflect.ValueOf(o.flip)
+	var params = []reflect.Value{x}
+	rs, skip, stop, e := userFuncCall(fv, params)
+
+	var item interface{} = rs[0].Interface()
+	if stop {
+		end = true
+		return
+	}
+	if skip {
+		return
+	}
+	if e != nil {
+		item = e
+	}
+
+	// send data
+	if !end {
+		if b, ok := item.(bool); ok && b {
+			end = o.sendToFlow(ctx, x.Interface(), out)
+		}
+		// if item != nil && item == x.Interface() {
+		// 	end = o.sendToFlow(ctx, x.Interface(), out)
+		// }
+		// end = o.sendToFlow(ctx, item, out)
+	}
+
+	return
+}}
+
+//Homework, Skip
+
+func (parent *Observable) Take(count int) (o *Observable) {
+	// check validation of f
+	i := 0
+	fv := reflect.ValueOf(
+		func(x interface{}) bool {
+			i += 1
+			return i <= count
+		})
+	inType := []reflect.Type{typeAny}
+	outType := []reflect.Type{typeBool}
+	b, ctx_sup := checkFuncUpcast(fv, inType, outType, true)
+	if !b {
+		panic(ErrFuncFlip)
+	}
+
+	o = parent.newTransformObservable("distinct")
+	o.flip_accept_error = checkFuncAcceptError(fv)
+
+	o.flip_sup_ctx = ctx_sup
+	o.flip = fv.Interface()
+	o.operator = takeOperater
+	return o
+}
+
+var takeOperater = transOperater{func(ctx context.Context, o *Observable, x reflect.Value, out chan interface{}) (end bool) {
+
+	fv := reflect.ValueOf(o.flip)
+	var params = []reflect.Value{x}
+	rs, skip, stop, e := userFuncCall(fv, params)
+
+	var item interface{} = rs[0].Interface()
+	if stop {
+		end = true
+		return
+	}
+	if skip {
+		return
+	}
+	if e != nil {
+		item = e
+	}
+
+	// send data
+	if !end {
+		if b, ok := item.(bool); ok && b {
+			end = o.sendToFlow(ctx, x.Interface(), out)
+		}
+		// if item != nil && item == x.Interface() {
+		// 	end = o.sendToFlow(ctx, x.Interface(), out)
+		// }
+		// end = o.sendToFlow(ctx, item, out)
+	}
+
+	return
+}}
+
+//Homework, Debounce
+
+func (parent *Observable) Debounce(wait_time time.Duration) (o *Observable) {
+	// check validation of f
+	fv := reflect.ValueOf(
+		func(x interface{}) bool {
+			return true
+		})
+	inType := []reflect.Type{typeAny}
+	outType := []reflect.Type{typeBool}
+	b, ctx_sup := checkFuncUpcast(fv, inType, outType, true)
+	if !b {
+		panic(ErrFuncFlip)
+	}
+
+	o = parent.newTransformObservable("Debounce")
+	o.flip_accept_error = checkFuncAcceptError(fv)
+	o.threading = ThreadingComputing
+	o.flip_sup_ctx = ctx_sup
+	o.flip = fv.Interface()
+	i := 0
+	o.operator = transOperater{
+		func(ctx context.Context, o *Observable, item reflect.Value, out chan interface{}) (end bool) {
+			i++
+			var tempCount = i
+			time.Sleep(wait_time)
+			// time.Sleep(time.Second)
+			if tempCount == i {
+				end = o.sendToFlow(ctx, item.Interface(), out)
+			}
+			return
+		}}
+	return o
+}
+
+//Homework IgnoreElements
+
+func (parent *Observable) IgnoreElements() (o *Observable) {
+	// check validation of f
+	fv := reflect.ValueOf(
+		func(x interface{}) bool {
+			return false
+		})
+	inType := []reflect.Type{typeAny}
+	outType := []reflect.Type{typeBool}
+	b, ctx_sup := checkFuncUpcast(fv, inType, outType, true)
+	if !b {
+		panic(ErrFuncFlip)
+	}
+
+	o = parent.newTransformObservable("IgnoreElements")
+	o.flip_accept_error = checkFuncAcceptError(fv)
+
+	o.flip_sup_ctx = ctx_sup
+	o.flip = fv.Interface()
+	o.operator = ignoreElementsOperater
+	return o
+}
+
+var ignoreElementsOperater = transOperater{func(ctx context.Context, o *Observable, x reflect.Value, out chan interface{}) (end bool) {
+	return false
+}}
+
+func (parent *Observable) Last() (o *Observable) {
+	o = parent.newTransformObservable("last")
+	o.flip_accept_error = false
+	o.flip_sup_ctx = false
+	o.flip = nil
+	temp := make([]reflect.Value, 1)
+	o.operator = myOperater{
+		func(ctx context.Context, o *Observable, item reflect.Value, out chan interface{}) (end bool) {
+			temp[0] = item
+			o.flip = temp
+			return false
+		}}
+	return o
+}
+
+func (parent *Observable) Skiplast(n int) (o *Observable) {
+	o = parent.newTransformObservable("Skiplast")
+	o.flip_accept_error = false
+	o.flip_sup_ctx = false
+	o.flip = nil
+	var temp []reflect.Value
+	var tempcount = 0
+	o.operator = myOperater{
+		func(ctx context.Context, o *Observable, item reflect.Value, out chan interface{}) (end bool) {
+			if tempcount == n {
+				end = o.sendToFlow(ctx, temp[0].Interface(), out)
+				temp = temp[1:]
+			} else {
+				tempcount++
+			}
+			temp = append(temp, item)
+			return
+		}}
+	return o
+}
+
+func (parent *Observable) Takelast(n int) (o *Observable) {
+	o = parent.newTransformObservable("Takelast")
+	o.flip_accept_error = false
+	o.flip_sup_ctx = false
+	o.flip = nil
+	var temp []reflect.Value
+	var tempcount = 0
+	o.operator = myOperater{
+		func(ctx context.Context, o *Observable, item reflect.Value, out chan interface{}) (end bool) {
+			if tempcount == n {
+				temp = temp[1:]
+			} else {
+				tempcount++
+			}
+			temp = append(temp, item)
+			o.flip = temp
+			return
+		}}
+	return o
+}
